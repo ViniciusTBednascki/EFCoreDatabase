fee9b98df40d8903fce36f8e067ff37a71c3e0f1
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2022-06-16 21:12:10 -0300

    making an initial cli

--- Program.cs
+++ Program.cs
@@ -2,6 +2,21 @@
 
 using (var db = new DataBaseContext())
 {
-  Console.WriteLine("Garantido que o banco de dados esteja criado...");
-  db.Database.EnsureCreated();
+  if(args.Length == 0)
+  {
+    Console.WriteLine("Description:\n  Database project using Entity Framework\n");
+    Console.WriteLine("Usage:\n  dotnet run -- [options]\n");
+    Console.WriteLine("Options:");
+    Console.WriteLine(" create\t\t\t\t Create the template database");
+    Console.WriteLine(" add <table> <values>\t\t Add a new value to table");
+    Console.WriteLine(" update <table> <values>\t Update a value in table");
+    Console.WriteLine(" delete <table> <values>\t Delete a value in table");
+    return;
+  }
+
+  if(args[0] == "create")
+  {
+    Console.WriteLine("Creating template database...");
+    db.Database.EnsureCreated();
+  }
 }
